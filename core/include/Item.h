155977a00c684842a48f22a6e0c97992b516aa33
Ani <sparkra1n@proton.me>
2026-01-06 00:40:42 -0800

    Fill out Item struct

--- core/include/Item.h
+++ core/include/Item.h
@@ -1,9 +1,21 @@
 #pragma once
-
 #include "Chireiden.h"
+#include "AnmManager.h"
+#include "Timer.h"
 #include "Macros.h"
 
 struct Item
 {
-
+	AnmVm vm;
+	D3DXVECTOR3 vec3;
+	D3DXVECTOR2 vec2;
+	int someInt_0x448;
+	int someInt_0x44c;
+	Timer timer;
+	int someInt_0x464;
+	int someInt_0x468;
+	int someInt_0x46c;
+	int shotFileRelated;
+	int someInt_0x474;
 };
+ASSERT_SIZE(Item, 0x478);
