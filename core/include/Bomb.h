63d6af4e785b02251bc6d73689d2ef0d33010b3b
Ani <sparkra1n@proton.me>
2026-01-06 00:40:06 -0800

    Fill out Bomb struct

--- core/include/Bomb.h
+++ core/include/Bomb.h
@@ -1,6 +1,27 @@
 #pragma once
+#include "AnmVm.h"
+#include "AnmId.h"
+#include "Chain.h"
+#include "DistortionMesh.h"
 
-class Bomb
+struct Bomb
 {
-    
+    int idk0;
+    int idk1;
+    ChainElem* onTick;
+    ChainElem* onDraw;
+    int idk2;
+    Timer timer0;
+    Timer timer1;
+    BOOL isUsingBomb;
+    AnmId vmId;
+    int playerAnmSlotIndex;
+    AnmVm vm;
+    void* unknownHeapAllocated;
+    DistortionMesh* distortionMesh;
+    D3DXVECTOR3 playerPos;
+    D3DXVECTOR2 someVec2;
+    uint32_t someIndicator;
+    int idk3;
 };
+ASSERT_SIZE(Bomb, 0x4a0);
