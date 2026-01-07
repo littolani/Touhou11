7ae6863db1dd5bd94c43567dcb7cd615c1d7e19c
Ani <sparkra1n@proton.me>
2026-01-07 00:30:26 -0800

    Fill in ItemManager struct

--- core/include/ItemManager.h
+++ core/include/ItemManager.h
@@ -1,9 +1,20 @@
 #pragma once
-
 #include "Chireiden.h"
+#include "Chain.h"
+#include "Item.h"
 #include "Macros.h"
 
 class ItemManager
 {
-
+	uint32_t flags;
+	uint32_t idk1;
+	ChainElem* onTick;
+	ChainElem* onDraw;
+	uint32_t idk2;
+	Item normalItems[150];
+	Item cancelItems[2048];
+	int numItemsAlive;
+	int nextCancelItemIndex;
+	int numCancelledItemsSpawnedThisFrame;
 };
+ASSERT_SIZE(ItemManager, 0x265e70);
