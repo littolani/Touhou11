1cc7d036b9d278fe0218da3b09645bd8302e73a5
Ani <sparkra1n@proton.me>
2026-01-07 23:54:03 -0800

    add RenderVertexSq

--- core/include/RenderVertex.h
+++ core/include/RenderVertex.h
@@ -9,3 +9,10 @@ struct RenderVertex144
     D3DXVECTOR2 uv;
 };
 ASSERT_SIZE(RenderVertex144, 0x1c);
+
+struct RenderVertexSq
+{
+    D3DXVECTOR3 position;
+    D3DXVECTOR2 uv;
+};
+ASSERT_SIZE(RenderVertexSq, 0x14);
