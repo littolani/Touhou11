cd97680aecdf6ca5ac14cd4fb7309e430c1bb318
Ani <sparkra1n@proton.me>
2026-01-15 03:47:49 -0800

    Add reopen, read, resetFile

--- core/include/CWaveFile.h
+++ core/include/CWaveFile.h
@@ -21,16 +21,14 @@ public:
     DWORD dwSize;
     int idk1[18];
     ULONG dataSize;
-    DWORD someNumber;
-    uint32_t m_ulDataSize;
-    uint32_t totalSize;
-    int bgmLength;
-    HANDLE fileHandle;
+    BOOL m_readMode;
+    byte* m_memBufferStart;
+    byte* m_memBufferCurrent;
+    DWORD m_memBufferLength;
+    HANDLE m_fileHandle;
     ThBgmFormat* thBgmFormat;
     LPCSTR audioFilename;
     int filePointer;
-
-    // CWaveFile* This, ThBgmFormat* thBgmFormat, const char* filepath
 
     /**
      * 0x45b220
@@ -40,6 +38,37 @@ public:
      * @param  filepath    EDI:4
      * @return HRESULT     EAX:4
      */
-    static HRESULT openWavFile(CWaveFile* This, ThBgmFormat* thBgmFormat, const char* filepath);
+    static HRESULT open(CWaveFile* This, ThBgmFormat* thBgmFormat, const char* filepath);
+
+    /**
+     * 0x45b480
+     * @brief
+     * @param  This         ESI:4
+     * @param  pBuffer      Stack[0x4]:4
+     * @param  pdwSizeRead  Stack[0x8]:4
+     * @param  dwSizeToRead EAX:4
+     * @return HRESULT      EAX:4
+     */
+    static HRESULT read(CWaveFile* This, BYTE* pBuffer, DWORD* pdwSizeRead, DWORD dwSizeToRead);
+
+    /**
+     * 0x45b2d0
+     * @brief
+     * @param  This        ECX:4
+     * @param  thBgmFormat EAX:4
+     * @param  seekOffset  Stack[0x4]:4
+     * @return HRESULT     EAX:4
+     */
+    static HRESULT reopen(CWaveFile* This, ThBgmFormat* thBgmFormat, int seekOffset);
+
+    /**
+     * 0x45b3a0
+     * @brief
+     * @param  This         ESI:4
+     * @param  loop         BL:1
+     * @param  seekOffset   EDI:4
+     * @return HRESULT      EAX:4
+     */
+    static HRESULT resetFile(CWaveFile* This, bool loop, int seekOffset);
 };
 ASSERT_SIZE(CWaveFile, 0x9c); // Verified
--- core/src/CWaveFile.cpp
+++ core/src/CWaveFile.cpp
@@ -1,12 +1,12 @@
 #include "CWaveFile.h"
 #include "Globals.h"
 
-HRESULT CWaveFile::openWavFile(CWaveFile* This, ThBgmFormat* thBgmFormat, const char* filepath)
+HRESULT CWaveFile::open(CWaveFile* This, ThBgmFormat* thBgmFormat, const char* filepath)
 {
     if (!filepath)
         return E_INVALIDARG;
 
-    printf("CWaveFile Constructor: %s\n", filepath);
+    printf("CWaveFile open: %s\n", filepath);
 
     HANDLE hFile = CreateFileA(
         filepath,
@@ -19,13 +19,13 @@ HRESULT CWaveFile::openWavFile(CWaveFile* This, ThBgmFormat* thBgmFormat, const 
         NULL
     );
 
-    This->fileHandle = hFile;
+    This->m_fileHandle = hFile;
     if (hFile == INVALID_HANDLE_VALUE)
         return E_FAIL;
 
     This->thBgmFormat = thBgmFormat;
     This->audioFilename = filepath;
-    if (This->someNumber == 0)
+    if (This->m_readMode == 0)
     {
         if (hFile)
         {
@@ -35,11 +35,132 @@ HRESULT CWaveFile::openWavFile(CWaveFile* This, ThBgmFormat* thBgmFormat, const 
     }
     else
     {
-        This->totalSize = This->m_ulDataSize;
+        This->m_memBufferCurrent = This->m_memBufferStart;
         if (thBgmFormat->totalLength > 0)
-            This->bgmLength = thBgmFormat->totalLength;
+            This->m_memBufferLength = thBgmFormat->totalLength;
     }
     This->dwSize = This->mmckinfo.cksize;
     return 0;
 }
+
+
+HRESULT CWaveFile::reopen(CWaveFile* This, ThBgmFormat* thBgmFormat, int seekOffset)
+{
+    puts("CWaveFile reopen");
+    if (This->m_readMode != 0)
+        return E_FAIL;
+
+    if (This->m_fileHandle == INVALID_HANDLE_VALUE)
+    {
+        This->dataSize = 1;
+        This->m_readMode = 0;
+        open(This, thBgmFormat, This->audioFilename);
+        if (This->m_fileHandle == INVALID_HANDLE_VALUE)
+            return E_FAIL;
+    }
+    This->thBgmFormat = thBgmFormat;
+    resetFile(This, false, seekOffset);
+    This->dwSize = (This->mmckinfo).cksize;
+    return S_OK;
+}
+
+HRESULT CWaveFile::read(CWaveFile* This, BYTE* pBuffer, DWORD* pdwSizeRead, DWORD dwSizeToRead)
+{
+    if (This->m_readMode == 0)
+    {
+        HANDLE hFile = This->m_fileHandle;
+
+        if (!hFile) return
+            CO_E_NOTINITIALIZED;
+
+        if (!pBuffer || !pdwSizeRead)
+            return E_INVALIDARG;
+
+        DWORD chunkSize = This->mmckinfo.cksize;
+
+        if (dwSizeToRead > chunkSize)
+            dwSizeToRead = chunkSize;
+
+        This->mmckinfo.cksize = chunkSize - dwSizeToRead;
+        DWORD bytesRead = 0;
+        ReadFile(hFile, pBuffer, dwSizeToRead, &bytesRead, NULL);
+
+        if (pdwSizeRead)
+            *pdwSizeRead = bytesRead;
+
+        return S_OK;
+    }
+    else
+    {
+        byte* currentPtr = This->m_memBufferCurrent;
+
+        if (!currentPtr)
+            return CO_E_NOTINITIALIZED;
+
+        if (pdwSizeRead)
+            *pdwSizeRead = 0;
+
+        byte* bufferEnd = This->m_memBufferStart + This->m_memBufferLength;
+        byte* targetEnd = currentPtr + dwSizeToRead;
+
+        if (targetEnd > bufferEnd)
+            dwSizeToRead = (DWORD)(bufferEnd - currentPtr);
+
+        memcpy(pBuffer, currentPtr, dwSizeToRead);
+
+        This->m_memBufferCurrent = currentPtr + dwSizeToRead;
+
+        if (pdwSizeRead)
+            *pdwSizeRead = dwSizeToRead;
+
+        return S_OK;
+    }
+}
+
+HRESULT CWaveFile::resetFile(CWaveFile* This, bool loop, int seekOffset)
+{
+    if (This->m_readMode == 0)
+    {
+        HANDLE hFile = This->m_fileHandle;
+        if (hFile == NULL)
+            return CO_E_NOTINITIALIZED;
+
+        DWORD movePos = 0;
+        DWORD chunkSize = 0;
+
+        if (loop && This->thBgmFormat->introLength > 0)
+        {
+            movePos = This->thBgmFormat->startOffset + This->thBgmFormat->introLength;
+            chunkSize = This->thBgmFormat->totalLength - This->thBgmFormat->introLength;
+        }
+
+        else if (seekOffset == 0)
+        {
+            movePos = This->thBgmFormat->startOffset;
+            chunkSize = This->thBgmFormat->totalLength;
+        }
+        else
+        {
+            movePos = seekOffset;
+            chunkSize = (This->thBgmFormat->totalLength + This->thBgmFormat->startOffset) - seekOffset;
+        }
+
+        movePos += g_soundManager.someWaveFileOffset;
+        SetFilePointer(hFile, movePos, NULL, FILE_BEGIN);
+        This->mmckinfo.cksize = chunkSize;
+    }
+    else
+    {
+        This->m_memBufferCurrent = This->m_memBufferStart;
+
+        if (This->thBgmFormat->totalLength > 0)
+            This->m_memBufferLength = This->thBgmFormat->totalLength;
+
+        if (loop && This->thBgmFormat->introLength > 0)
+            This->m_memBufferCurrent += This->thBgmFormat->introLength;
+    }
+
+    return S_OK;
+}
+
 
