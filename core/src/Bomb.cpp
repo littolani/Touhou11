18f40b8458ff6662a19b87502050ff69ebfd93d7
Ani <sparkra1n@proton.me>
2026-01-21 23:57:17 -0800

    Draft onTickReimuA

--- core/src/Bomb.cpp
+++ core/src/Bomb.cpp
@@ -1,13 +1,17 @@
+#include "AnmVm.h"
+#include "AnmManager.h"
 #include "Bomb.h"
+#include "Player.h"
 #include "Shottypes.h"
+#include "SoundManager.h"
 
 ChainCallbackResult Bomb::onTick(Bomb* This)
 {
+    puts("bomb on tick\n");
     if (This->isUsingBomb == FALSE)
         return CHAIN_CALLBACK_RESULT_CONTINUE;
 
     int result = 0;
-
     switch (g_globals.character)
     {
     case CharacterId::Reimu:
@@ -56,6 +60,30 @@ ChainCallbackResult Bomb::onTick(Bomb* This)
 
 int Bomb::onTickReimuA(Bomb* This)
 {
+    AnmVm* vm = g_anmManager->getVmWithId(g_anmManager, (This->vmId).id);
+    if (!vm)
+    {
+        This->vmId.id = 0;
+        g_player->vm0.loadIntoAnmVm(&g_player->vm0, g_player->playerAnm, 0);
+        g_player->someFlag &= 0xfffffffd;
+        g_player->attemptedVelocityInternal.x = 0;
+        g_player->attemptedVelocityInternal.y = 0;
+        g_player->setIframes(0x28);
+        return -1;
+    }
+
+    int currentTime = This->timer0.m_current;
+    if (currentTime > 179)
+    {
+        //if (currentTime == 180)
+        //{
+        //    g_soundManager.playSoundCentered(0x26);
+        //    g_soundManager.stopBombReimuAB();
+        //    g_player->createDamageSource(g_player, &This->playerPos, 32.0, 10.0, 0x1e, 0x32);
+        //}
+        //This->sub_0045df10(This);
+        //(This->someVec2).x = (This->someVec2).y + (This->someVec2).x;
+    }
     return 0;
 }
 
